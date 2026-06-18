0dc7d765786eac324eec0bc70bfe18f802f5007b
hitesh-bhusara <hitechbhusara@gmail.com>
2026-06-18 17:37:44 +0530

    add comments to showcase the process of TDD

--- 08_test_driven_development/app.js
+++ 08_test_driven_development/app.js
@@ -1,10 +1,17 @@
 export function normalizeInput(input){
+
+    // return empty string if input does not exists
     if(!input) return '';
+
     let result = input;
+
+    // trims the spaces
     result = input.trim();
 
+    // removes extra whitespaces
     result = result.replace(/\s+/, ' ');
-
+    
+    // capatalizes the input
     result = result
         .split(' ')
         .filter(Boolean)
